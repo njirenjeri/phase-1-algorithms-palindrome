a95a9d1aeaf1aa36474b45600bf587df62a28177
Njire Njeri <njirenjeri@gmail.com>
2025-01-01 15:39:08 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // 1. take the word 
+  // 2. reverse the word 
+  // 3. compare the two words 
+  // 4. return true if its the same and false if it's not
+  let newWord = word.split('').reverse().join('');
+  // console.log(newWord)
+
+  if(newWord.toLowerCase() === word.toLowerCase()){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /* 
@@ -8,6 +20,12 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  --we use the .split('') method with the quote marks to split the word at every character and 
+  turns it to an array of characters
+  --then we can apply the .reverse() array method to reverse the characters
+  --then use the .join() method to join the characters back to a word
+
+  --use the if block to compare the words to see if they are the same
 */
 
 // You can run `node index.js` to view these console logs
